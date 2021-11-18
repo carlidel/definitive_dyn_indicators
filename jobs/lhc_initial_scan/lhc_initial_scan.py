428dcb30852121dc7c12026cb6145d7de3cc029b
Carlo Emilio Montanari <carlidel95@gmail.com>
2021-11-18 17:28:46 +0100

    verbosity

--- jobs/lhc_initial_scan/lhc_initial_scan.py
+++ jobs/lhc_initial_scan/lhc_initial_scan.py
@@ -19,9 +19,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    print("Creating the engine.")
     engine = xtrack_engine(context="CUDA", line_path=args.input)
 
     # Load data
+    print("Loading data.")
     samples = lhc_config["samples"]
 
     x_flat = lhc_config["x_flat"]
@@ -30,10 +32,19 @@ if __name__ == '__main__':
     py_flat = lhc_config["py_flat"]
     t = lhc_config["long_tracking"]
 
-    # Run engine
-    x, px, y, py, steps = engine.track(x_flat, px_flat, y_flat, py_flat, t)
+    print("x_flat size:", x_flat.shape)
+    print("px_flat size:", px_flat.shape)
+    print("y_flat size:", y_flat.shape)
+    print("py_flat size:", py_flat.shape)
 
+    # Run engine
+    print("Running the engine.")
+    start = time.perf_counter()
+    x, px, y, py, steps = engine.track(x_flat, px_flat, y_flat, py_flat, t)
+    end = time.perf_counter()
+    print("Time: {}".format(end - start))
     # Format data
+    print("Formatting data.")
     x = x.reshape(samples, -1)
     px = px.reshape(samples, -1)
     y = y.reshape(samples, -1)
@@ -46,6 +57,7 @@ if __name__ == '__main__':
     filename = os.path.splitext(filename)[0]
     # compose output filename
     output_filename = "initial_scan_{filename}.pkl"
+    print("Saving data to {}".format(output_filename))
 
     # Save data
     with open(os.path.join(OUTDIR, output_filename), 'wb') as f:
