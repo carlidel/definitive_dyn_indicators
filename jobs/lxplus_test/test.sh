bef332b7ab70a621004a9ec81c6644e39e0e2a21
Carlo Emilio Montanari <carlidel95@gmail.com>
2021-11-18 17:45:13 +0100

    more!!

--- jobs/lxplus_test/test.sh
+++ jobs/lxplus_test/test.sh
@@ -2,3 +2,9 @@
 
 # execute nvidia-smi to get the GPU information and pipe it to a file
 nvidia-smi | nvidia-smi.txt
+
+# execute nvidia-smi again to get the GPU information and pipe it to stderr
+nvidia-smi 2>&1
+
+# execute nvidia-smi again to get the GPU information and pipe it to stdout
+nvidia-smi >&1
