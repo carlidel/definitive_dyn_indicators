2641d65c760e436b1a48425563c9f13a4829ed16
Carlo Emilio Montanari <carlidel95@gmail.com>
2021-12-01 16:10:56 +0100

    had to split the job...

---
+++ jobs/tools/compose_dagman.py
@@ -0,0 +1,22 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compose a DAGMan file from a list of arguments")
+    parser.add_argument("-n", "--number", type=int, help="Number of jobs to run", required=True)
+    parser.add_argument("-f", "--first", type=str, help="First job to run", required=True)
+    parser.add_argument("-r", "--rest", type=str, help="Rest of the jobs to run", required=True)
+    
+    args = parser.parse_args()
+
+    # create list of random names of lenght args.number
+    names = [''.join(["job", str(i)]).upper() for i in range(args.number)]
+
+    # write in the dag file
+    with open("dagman.dag", "w") as dagman:
+        for i, n in enumerate(names):
+            dagman.write("JOB {} {}\n".format(n, args.first if i == 0 else args.rest))
+
+        dagman.write("\n")
+
+        for i, n in enumerate(names[:-1]):
+            dagman.write("PARENT {} CHILD {}\n".format(n, names[i+1])) 
