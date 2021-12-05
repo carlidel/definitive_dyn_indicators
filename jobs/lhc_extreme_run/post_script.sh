13288e090992d7fade6fef24fc800d38dad1c83a
Carlo Emilio Montanari <carlidel95@gmail.com>
2021-12-05 23:32:21 +0100

    this is hell

--- jobs/lhc_extreme_run/post_script.sh
+++ jobs/lhc_extreme_run/post_script.sh
@@ -3,4 +3,4 @@ export EOS_MGM_URL=root://eosuser.cern.ch
 
 eos cp /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/scan*basic*.pkl /eos/project/d/da-and-diffusion-studies/DA_Studies/Simulations/Models/lhc_track_simulations
 
-rm -v /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/scan*basic*.pkl
+rm -v /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/scan*basic*nturns*.pkl
--- jobs/lhc_track_displacements/post_script.sh
+++ jobs/lhc_track_displacements/post_script.sh
@@ -3,4 +3,4 @@ export EOS_MGM_URL=root://eosuser.cern.ch
 
 eos cp /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/scan*advanced*.pkl /eos/project/d/da-and-diffusion-studies/DA_Studies/Simulations/Models/lhc_track_simulations
 
-rm -v /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/scan*advanced*.pkl
+rm -v /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/scan*advanced*nturns*.pkl
