e039bf4523a8283761acc8d17f57e51736f89e27
Carlo Emilio Montanari <carlidel95@gmail.com>
2021-12-06 00:52:21 +0100

    better?

--- jobs/lhc_extreme_run/post_script.sh
+++ jobs/lhc_extreme_run/post_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export EOS_MGM_URL=root://eosuser.cern.ch
 
-eos cp /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/scan*basic*nturns*.pkl /eos/project/d/da-and-diffusion-studies/DA_Studies/Simulations/Models/lhc_track_simulations
+eos cp $1 /eos/project/d/da-and-diffusion-studies/DA_Studies/Simulations/Models/lhc_track_simulations
 
-rm -v /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/scan*basic*nturns*.pkl
+rm -v $1
