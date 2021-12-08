4987f67cc7e0b43d9087857554e5d8474ad86563
Carlo Emilio Montanari <carlidel95@gmail.com>
2021-12-08 14:56:31 +0100

    what the hell is happening??

--- jobs/lhc_extreme_run/execute_first_run.sh
+++ jobs/lhc_extreme_run/execute_first_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 pwd
 ls -alF
-nvidia-smi
+echo $1  >> /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/jobs/lhc_extreme_run/log/log/nvidia-smi.txt
+echo $2  >> /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/jobs/lhc_extreme_run/log/log/nvidia-smi.txt
+nvidia-smi >> /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/jobs/lhc_extreme_run/log/log/nvidia-smi.txt
 python3 generic_run.py --input $1 --zeta-option $2 --displacement-kind none --time-step basic
