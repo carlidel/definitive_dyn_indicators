c980dbbb44daa34cd9328fb9e8f70c16ca0fad93
Carlo Emilio Montanari <carlidel95@gmail.com>
2022-01-07 17:15:42 +0100

    this is terrible...

--- jobs/tools/move_data.py
+++ jobs/tools/move_data.py
@@ -1,9 +1,9 @@
 import os
+import subprocess
 from tqdm import tqdm
 
 DATADIR = "/afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/"
 OUTDIR = "/eos/project/d/da-and-diffusion-studies/DA_Studies/Simulations/Models/dynamic_indicator_analysis/big_data"
-NOEOS = False
 
 # read file finished.txt from datadir
 with open(os.path.join(DATADIR, "finished.txt"), "r") as f:
@@ -20,13 +20,16 @@ try:
     for f in tqdm(finished):
         print("copying {}".format(f))
         # perform a system command
-        if NOEOS:
-            os.system(
-                f"cp {os.path.join(DATADIR, f)} {os.path.join(OUTDIR, f)}")
-        else:
-            os.system(
-                f"eos cp {os.path.join(DATADIR, f)} {os.path.join(OUTDIR, f)}")
+        subprocess.run(
+            [
+                "cp", 
+                f"{os.path.join(DATADIR, f)}", 
+                f"{os.path.join(OUTDIR, f)}",
+                "-v"
+            ],
+            check=True)
         # remove the file from the list finished
+        print("Probably done...")
         print("removing {}".format(f))
         os.remove(os.path.join(DATADIR, f))
         files_left.remove(f)
