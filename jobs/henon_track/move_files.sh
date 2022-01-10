163125c35e052f08345728fb8bc27dfa085aaa4f
Carlo Emilio Montanari <carlidel95@gmail.com>
2022-01-10 19:25:31 +0100

    other corrections for data transfer

--- jobs/henon_track/move_files.sh
+++ jobs/henon_track/move_files.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export EOS_MGM_URL=root://eosuser.cern.ch
 eos cp /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/$1 /eos/project/d/da-and-diffusion-studies/DA_Studies/Simulations/Models/dynamic_indicator_analysis/big_data/
 rm -v /afs/cern.ch/work/c/camontan/public/definitive_dyn_indicators/data/$1
