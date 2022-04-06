a289993cf92baeaa6bee88131125e374ce9cab12
Carlo Emilio Montanari <carlidel95@gmail.com>
2022-04-06 15:23:41 +0200

    First attempt for htcondor...

--- jobs/IPAC_LHC/run.py
+++ jobs/IPAC_LHC/run.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     parser.add_argument("--hdf5_filename", type=str, required=True)
     parser.add_argument("--checkpoint_filename", type=str, required=True)
     parser.add_argument("--hl_lhc", type=int, default=0, help="configuration index, 0-5")
+    parser.add_argument("--particle_config", type=int, default=0, choices=[0, 1, 2])
 
     parser.add_argument("--dyn_ind", type=str, 
         choices=["ground_truth", "fli", "rem", "ofli", "sali", "gali4", "gali6", "tune"]
@@ -30,6 +31,8 @@ if __name__ == "__main__":
     else:
         context = xo.ContextCpu()
 
+    particle_config = cfg.particle_config_low[args.particle_config]
+
     eos_config = cfg.default_eos
     eos_config.hdf5_filename = args.hdf5_filename
     eos_config.checkpoint_filename = args.checkpoint_filename
@@ -42,40 +45,40 @@ if __name__ == "__main__":
     else:
         p_list = []
         p_list.append(xp.Particles(
-            cfg.particle_config_low.get_initial_codintions()).to_dict())
+            particle_config.get_initial_codintions()).to_dict())
         if args.dyn_ind == "ground_truth" or args.dyn_ind == "fli":
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "random_4d")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "random_4d")).to_dict())
         elif args.dyn_ind == "ofli" or args.dyn_ind == "gali6":
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "x")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "x")).to_dict())
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "px")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "px")).to_dict())
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "y")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "y")).to_dict())
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "py")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "py")).to_dict())
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "z")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "z")).to_dict())
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "delta")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "delta")).to_dict())
         elif args.dyn_ind == "gali4":
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "x")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "x")).to_dict())
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "px")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "px")).to_dict())
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "y")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "y")).to_dict())
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "py")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "py")).to_dict())
         elif args.dyn_ind == "sali":
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "x")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "x")).to_dict())
             p_list.append(xp.Particles(
-                cfg.particle_config_mid.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "y")).to_dict())
+                particle_config.get_initial_conditions_with_displacement(cfg.run_config_dyn_indicator.displacement_module, "y")).to_dict())
 
         chk = xe.Checkpoint(
-            particles_config=cfg.particle_config_low,
+            particles_config=particle_config,
             lhc_config=cfg.lhc_configs[args.hl_lhc],
             run_config=cfg.run_config_ground_truth if args.dyn_ind == "ground_truth" else cfg.run_config_dyn_indicator,
             eos_config=eos_config,
